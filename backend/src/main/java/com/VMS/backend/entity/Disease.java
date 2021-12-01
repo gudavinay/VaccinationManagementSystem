cdcd548eff3906e0acf1145576464a7156dff7b6
payalghule <payalghule@gmail.com>
2021-12-01 13:29:29 -0800

    entity- pojo classes

---
+++ backend/src/main/java/com/VMS/backend/entity/Disease.java
@@ -0,0 +1,28 @@
+package com.VMS.backend.entity;
+
+public class Disease {
+    private String diseaseName;
+    private int diseaseId;
+    private String diseaseDesc;
+    public String getDiseaseName() {
+        return diseaseName;
+    }
+    public void setDiseaseName(String diseaseName) {
+        this.diseaseName = diseaseName;
+    }
+    public int getDiseaseId() {
+        return diseaseId;
+    }
+    public void setDiseaseId(int diseaseId) {
+        this.diseaseId = diseaseId;
+    }
+    public String getDiseaseDesc() {
+        return diseaseDesc;
+    }
+    public void setDiseaseDesc(String diseaseDesc) {
+        this.diseaseDesc = diseaseDesc;
+    }
+    
+
+    
+}
---
+++ backend/src/main/java/com/VMS/backend/entity/Vaccination.java
@@ -0,0 +1,61 @@
+package com.VMS.backend.entity;
+
+import java.util.List;
+
+public class Vaccination {
+    private int vaccinationId;
+    private String vaccinationName;
+    private List<Disease> diseases;
+    private String manufacturer;
+    private int numberOfShots;
+    private int shotInternalVal;
+    private int duration;
+    public int getVaccinationId() {
+        return vaccinationId;
+    }
+    public void setVaccinationId(int vaccinationId) {
+        this.vaccinationId = vaccinationId;
+    }
+    public String getVaccinationName() {
+        return vaccinationName;
+    }
+    public void setVaccinationName(String vaccinationName) {
+        this.vaccinationName = vaccinationName;
+    }
+    public List<Disease> getDiseases() {
+        return diseases;
+    }
+    public void setDiseases(List<Disease> diseases) {
+        this.diseases = diseases;
+    }
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public int getNumberOfShots() {
+        return numberOfShots;
+    }
+    public void setNumberOfShots(int numberOfShots) {
+        this.numberOfShots = numberOfShots;
+    }
+    public int getShotInternalVal() {
+        return shotInternalVal;
+    }
+    public void setShotInternalVal(int shotInternalVal) {
+        this.shotInternalVal = shotInternalVal;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+
+
+
+
+    
+}
