6a94d3aab34430f2b6dcc2b458ff0e00b363c658
vinay guda <vinayguda05@gmail.com>
2021-12-06 19:31:50 -0800

    basic routes established

---
+++ frontend/src/components/Admin/AddVaccination/AddVaccination.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class AddVaccination extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                I'm in Add Vaccination
+            </React.Fragment>
+        );
+    }
+}
+
+export default AddVaccination;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import AddClinic from "./Admin/AddClinic/AddClinic";
+import AddDisease from "./Admin/AddDisease/Appointments";
+import AddVaccination from "./Admin/AddVaccination/AddVaccination";
+import Appointments from "./Home/Appointments/Appointments";
 import Dashboard from "./Home/Dashboard/Dashboard";
+import VaccinationHistory from "./Home/VaccinationHistory/VaccinationHistory";
+import VaccinationsDue from "./Home/VaccinationsDue/VaccinationsDue";
 import Navbar from "./Navbar/Navbar";
 import SignUp from "./SignUp/SignUp";
 
@@ -12,6 +18,12 @@ class Main extends Component {
           <Route exact path="/" component={SignUp} />
           <Route path="/signup" component={SignUp} />
           <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addClinic" component={AddClinic} />
+          <Route path="/addDisease" component={AddDisease} />
+          <Route path="/addVaccination" component={AddVaccination} />
+          <Route path="/appointments" component={Appointments} />
+          <Route path="/vaccinationHistory" component={VaccinationHistory} />
+          <Route path="/vaccinationsDue" component={VaccinationsDue} />
       </React.Fragment>
     );
   }
