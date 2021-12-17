6c2174b8c5085f4c7d9e3b15d4019575539b3c05
payalghule <payalghule@gmail.com>
2021-12-16 17:28:09 -0800

    aws config frontend

--- frontend/src/webConfig.js
+++ frontend/src/webConfig.js
@@ -1,3 +1,5 @@
-const backendServer = "http://localhost:8181";
+//const backendServer = "http://localhost:8181";
+
+const backendServer = "http://54.215.128.90:8181";
 
 export default backendServer;
